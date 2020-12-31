70674c6317b03fbe02d9c333515512bc2b4eeeef
Claudio Junior <Cjrnew@gmail.com>
2020-12-30 23:12:28 -0300

    Project created

---
+++ src/main/java/com/projectcontrolapi/ProjectcontrolapiApplication.java
@@ -0,0 +1,13 @@
+package com.projectcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcontrolapiApplication.class, args);
+	}
+
+}
