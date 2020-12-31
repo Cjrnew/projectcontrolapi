a62959356aaaa723ecde5069937159db04bbe296
Claudio Junior <Cjrnew@gmail.com>
2020-12-31 14:07:59 -0300

    Person service and rest CRUD.

--- src/main/java/com/projectcontrolapi/controller/rest/PersonRest.java
+++ src/main/java/com/projectcontrolapi/controller/rest/PersonRest.java
@@ -1,13 +1,20 @@
 package com.projectcontrolapi.controller.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projectcontrolapi.model.entities.Person;
 import com.projectcontrolapi.model.service.PersonService;
@@ -18,12 +25,37 @@ import com.projectcontrolapi.model.service.PersonService;
 public class PersonRest {
 	
 	@Autowired
-	private PersonService personRepository;
+	private PersonService personService;
 	
 	@GetMapping
 	public ResponseEntity<List<Person>> findAll() {
-		List<Person> list = personRepository.findAll();
+		List<Person> list = personService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Person> findById(@PathVariable Long id) {
+		Person obj = personService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Person> insert(@RequestBody Person obj) {
+		obj = personService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		personService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person obj) {
+		obj = personService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
--- src/main/java/com/projectcontrolapi/model/service/PersonService.java
+++ src/main/java/com/projectcontrolapi/model/service/PersonService.java
@@ -1,12 +1,19 @@
 package com.projectcontrolapi.model.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projectcontrolapi.model.entities.Person;
 import com.projectcontrolapi.model.repository.PersonRepository;
+import com.projectcontrolapi.model.service.exception.DatabaseException;
+import com.projectcontrolapi.model.service.exception.ResourceNotFoundException;
 
 @Service
 public class PersonService {
@@ -18,8 +25,38 @@ public class PersonService {
 		return personRepository.findAll();
 	}
 	
+	public Person findById(Long id) {
+		Optional<Person> obj = personRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
 	public Person insert(Person obj) {
 		return personRepository.save(obj);
+	}
+	
+public void delete(Long id) {
+		
+		try {
+			personRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Person update(Long id, Person obj) {		
+		try {
+			Person entity = personRepository.getOne(id);
+			updateData(entity, obj);
+			return personRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Person entity, Person obj) {
+		entity.setName(obj.getName());
 	}
 	
 }
